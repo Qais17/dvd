00f2f2362f5dd24d0a3392f4dbeff00a3cbd9294
RuperezRomain <ruperez.dev@gmail.com>
2017-06-02 12:01:42 +0200

    minor update redirection

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,5 +49,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['dvd'] = 'dvd';
+$route['dvd'] = 'Dvd/index';
 $route['default_controller'] = 'pages/view';
